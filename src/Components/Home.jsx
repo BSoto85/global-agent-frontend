57581d6b77426a110950e2c581d7333eb9df8c2f
armandopires <armandopires@pursuit.org>
2024-07-01 14:36:21 -0400

    Added navbar

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,36 +1,14 @@
 import React from 'react';
+import Navbar from './NavBar';
 import '../App.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome, faTrophy, faCogs, faMap, faShieldAlt, faPortrait } from '@fortawesome/free-solid-svg-icons';
+import { faPortrait } from '@fortawesome/free-solid-svg-icons';
 import detectiveImage from '../assets/vecteezy_detective-looking-through-case-board_22129502.svg';
 
 function Home() {
   return (
     <div className="App">
-      <header className="navbar">
-        <ul className="nav-links">
-          <li>
-            <a href="#home">
-              <FontAwesomeIcon icon={faHome} />
-            </a>
-          </li>
-          <li>
-            <a href="#leaderboard">
-              <FontAwesomeIcon icon={faTrophy} />
-            </a>
-          </li>
-          <li>
-            <a href="#map">
-              <FontAwesomeIcon icon={faMap} />
-            </a>
-          </li>
-          <li>
-            <a href="#shield">
-              <FontAwesomeIcon icon={faShieldAlt} />
-            </a>
-          </li>
-        </ul>
-      </header>
+      <Navbar /> 
       <main className="main-content">
         <div className="profile-icon-container">
           <FontAwesomeIcon icon={faPortrait} />
@@ -39,12 +17,11 @@ function Home() {
           <div className="image-container">
             <img src={detectiveImage} alt="Detective looking through case board" className="detective-image" />
           </div>
-          <div className='border-container'>
-            {/* <h1>Global Agent</h1> */}
+          <div className="border-container">
             <p>Welcome to Global Agent, where current events become your next big case! Join our elite team of young investigators to uncover the truth behind today's headlines! Are you ready to earn detective badges and rise through the ranks from Rookie to Master Sleuth & be a top-notch detective?</p>
           </div>
-          <div className='login-container'>
-            <div className='login-button'>YOUR MISSION AWAITS!</div>
+          <div className="login-container">
+            <div className="login-button">YOUR MISSION AWAITS!</div>
           </div>
         </section>
       </main>
---
+++ src/Pages/CaseFilesPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CaseFilesPage = () => {
+  return (
+    <div>CaseFilesPage</div>
+  )
+}
+
+export default CaseFilesPage
