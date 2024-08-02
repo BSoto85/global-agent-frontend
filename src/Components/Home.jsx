16658ed529242ea4dc90594a8c5bdb1712035171
brenda-soto <bsoto@pursuit.org>
2024-08-02 12:26:27 -0400

    Home CSS done for larger and smaller view

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,27 +3,20 @@ import { useNavigate, Link } from "react-router-dom";
 import "../CSS/Home.css";
 import detectiveImage from "../assets/vecteezy_detective-looking-through-case-board_22129502.svg";
 
-function Home({user}) {
+function Home({ user }) {
   const navigate = useNavigate();
 
   const handleLoginClick = () => {
-    if(user !== null){
-      navigate("/countries")
+    if (user !== null) {
+      navigate("/countries");
     } else {
-      navigate("/login")
+      navigate("/login");
     }
   };
   return (
     <div className="App">
       <main className="main-content">
         <section id="home" className="section">
-          <div className="image-container">
-            <img
-              src={detectiveImage}
-              alt="Detective looking through case board"
-              className="detective-image"
-            />
-          </div>
           <div className="border-container">
             <p>
               Welcome to Global Agent, where current events become your next big
@@ -35,6 +28,13 @@ function Home({user}) {
           </div>
           <div className="mission-button-container" onClick={handleLoginClick}>
             <div className="login-button">YOUR MISSION AWAITS!</div>
+          </div>
+          <div className="image-container">
+            <img
+              src={detectiveImage}
+              alt="Detective looking through case board"
+              className="detective-image"
+            />
           </div>
         </section>
       </main>
