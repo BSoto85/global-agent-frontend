f9f734e1bcb44c102fc1c3b5c20b18c45b657903
AnitaOwen <anitaowen@pursuit.org>
2024-08-02 15:37:34 -0400

    added confetti to results page

--- src/Pages/ResultPage.jsx
+++ src/Pages/ResultPage.jsx
@@ -1,26 +1,22 @@
 import React, { useState, useEffect } from "react";
-import { Link, useParams, useLocation } from "react-router-dom";
+import { Link, useParams, useLocation, useNavigate } from "react-router-dom";
 import ProgressReport from "../Components/ProgessReport";
 import "../CSS/ResultPage.css";
 const URL = import.meta.env.VITE_BASE_URL;
-
 const ResultsPage = ({ userStats, user, userProfile, setUserStats }) => {
   const { countryId, caseFileId } = useParams();
   const location = useLocation();
   const [currentStats, setCurrentStats] = useState(userStats);
   const [hasUpdated, setHasUpdated] = useState(false);
-
+  const navigate = useNavigate();
   const score = location.state?.score || 0;
   const totalQuestions = location.state?.totalQuestions || 0;
-
   useEffect(() => {
     if (userStats) {
       setCurrentStats(userStats);
     }
   }, [userStats]);
-
   const calculateXPEarned = () => (score === 4 ? 125 : score * 25)
-
   const updatePlayerStats = async (stats) => {
     try {
       const token = localStorage.getItem('token')
@@ -33,12 +29,10 @@ const ResultsPage = ({ userStats, user, userProfile, setUserStats }) => {
         body: JSON.stringify(stats),
       });
       console.log("response from UPDATE PLAYER STATS", response)
-
       if (!response.ok) {
         const errorText = await response.text();
         throw new Error(`Failed to update player stats: ${errorText}`);
       }
-
       const data = await response.json();
       console.log("data", data)
       console.log("USER STATS FROM RESULTS", userStats)
@@ -48,11 +42,9 @@ const ResultsPage = ({ userStats, user, userProfile, setUserStats }) => {
       console.error("Error updating player stats:", error);
     }
   };
-
   useEffect(() => {
     if (currentStats && !hasUpdated && score > 0) {
       const xpEarned = calculateXPEarned();
-
       const newCurrentStats = {
         ...currentStats,
         xp: currentStats.xp + xpEarned,
@@ -61,7 +53,6 @@ const ResultsPage = ({ userStats, user, userProfile, setUserStats }) => {
         questions_wrong: currentStats.questions_wrong + (totalQuestions - score),
       };
       setCurrentStats(newCurrentStats);
-
       const smallIncrement = {
         xp: xpEarned,
         games_played: 1,
@@ -72,22 +63,28 @@ const ResultsPage = ({ userStats, user, userProfile, setUserStats }) => {
       setHasUpdated(true);
     }
   }, [currentStats, score, totalQuestions, hasUpdated, userProfile]);
-
   // if (!currentStats) {
   //   return <p>Loading player stats...</p>;
   // }
-
+  const goToProfile = () => {
+    navigate(`/profile/${user.uid}`);
+  };
   return (
     <div className="ResultsPage">
       <h2>Case #{caseFileId}</h2>
       <div className="findings-border">
-        <h3>Results:</h3>
         <p>
           You got {score} out of 4 questions!
         </p>
+        {/* <h3>Results:</h3> */}
         {score === 4 && (
-        <>
-          <p>Wow you got them all correct!</p>
+          <>
+        <p>Wow you got them all correct!</p>
+        <img src="https://res.cloudinary.com/dgifdj6nx/image/upload/v1722620587/GlobalAgent-ConfettiOnlyNB_elnjhf.gif" alt="" />
+        <img src="https://res.cloudinary.com/dgifdj6nx/image/upload/v1722620587/GlobalAgent-ConfettiOnlyNB_elnjhf.gif" alt="" />
+        <img src="https://res.cloudinary.com/dgifdj6nx/image/upload/v1722620587/GlobalAgent-ConfettiOnlyNB_elnjhf.gif" alt="" />
+        <img className="confetti-image" src="https://res.cloudinary.com/dgifdj6nx/image/upload/v1722620587/GlobalAgent-ConfettiOnlyNB_elnjhf.gif" alt="" />
+        <img  className="confetti-image" src="https://res.cloudinary.com/dgifdj6nx/image/upload/v1722620587/GlobalAgent-ConfettiOnlyNB_elnjhf.gif" alt="" />
           <p>Bonus XP: 25</p>
         </>
       )}
@@ -103,18 +100,13 @@ const ResultsPage = ({ userStats, user, userProfile, setUserStats }) => {
           <Link to="/countries">
             <button className="retry-button">Start New Game</button>
           </Link>
+          <button className="retry-button" onClick={goToProfile}>
+        Go To Profile
+      </button>
         </div>
       </div>
       {<ProgressReport currentStats={currentStats} user={user}/>}
     </div>
   );
 };
-
 export default ResultsPage;
-
-
-
-
-
-
-
